8f36a53390f3a2a6f3a870dcce522ffa83169ce1
suhwan <soo7652@naver.com>
2019-09-09 13:19:29 +0900

    revise bike popup contents

---
+++ TwoBirdWithOneStone/app/src/main/java/com/example/twobirdwithonestone/Activity/PopupActivity1.java
@@ -0,0 +1,53 @@
+package com.example.twobirdwithonestone.Activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.example.twobirdwithonestone.R;
+
+public class PopupActivity1 extends Activity {
+
+    TextView txtText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_museum_popup);
+
+        txtText = (TextView)findViewById(R.id.txtText);
+
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("data");
+        txtText.setText(data);
+    }
+
+    public void mOnClose(View v){
+
+        Intent intent = new Intent();
+        intent.putExtra("result", "Close Popup");
+        setResult(RESULT_OK, intent);
+
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        return;
+    }
+}

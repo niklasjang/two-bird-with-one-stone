9fc8bbf97f93c2ecad39ff35352cc694c12e0cd4
Hz114 <kauphj17@gmail.com>
2019-09-28 03:05:46 +0900

    change fragment_home and fragment_gridview xml

---
+++ TwoBirdWithOneStone/app/src/main/java/com/example/twobirdwithonestone/Activity/SettingPopupActivity.java
@@ -0,0 +1,47 @@
+package com.example.twobirdwithonestone.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+
+import com.example.twobirdwithonestone.R;
+
+public class SettingPopupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_setting_popup);
+    }
+
+    public void mOnClose(View v){
+        //데이터 전달하기
+        Intent intent = new Intent();
+        intent.putExtra("result", "Close Popup");
+        setResult(RESULT_OK, intent);
+
+        //액티비티(팝업) 닫기
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        //바깥레이어 클릭시 안닫히게
+        if(event.getAction()==MotionEvent.ACTION_OUTSIDE){
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //안드로이드 백버튼 막기
+        return;
+    }
+}
